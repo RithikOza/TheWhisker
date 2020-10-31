a7c7ccb7735f2b979f3fdf4281b725cbd47e17c9
mysmyst <faizan.122000@gmail.com>
2020-10-31 18:13:13 +0000

    this isnt good

---
+++ the-whisker/src/ThePurposeofThisApp.js
@@ -0,0 +1,18 @@
+
+export default props => {
+  return (
+    <h2>nooooooooooo</h2>
+  );
+};
+
+function ThePurposeofThisApp() {
+  return (
+    <div className="ThePurposeofThisApp" id="outer-container">
+      <div>
+        <h2>well we dont know yet if youre asking but its gonna be cool, pretty cool</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ThePurposeofThisApp;
