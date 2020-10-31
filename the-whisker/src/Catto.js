778781510b88a590d4156c4e322a54a46cd2942f
mysmyst <faizan.122000@gmail.com>
2020-10-31 18:07:52 +0000

    cat added

---
+++ the-whisker/src/Catto.js
@@ -0,0 +1,16 @@
+class Catto extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Cat types:</h1>
+        {/* Change code below this line */}
+<Type1/>
+<Type2/>
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+};
